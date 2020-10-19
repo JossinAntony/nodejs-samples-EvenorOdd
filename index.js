fdf65a6b3e479c1a6b26f31682e0cc11bce163df
JossinAntony <jossinde@gmail.com>
2020-10-19 09:20:26 +0200

    write to file implemented.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var num = 0;
+const myfs = require('fs');
+var num = 14;
+var s;
 
 if(((num % 2) == 0) || (num == 0)){
+    s = num + ' is even.';
     console.log(num + ' is even.');
 }else{
+    s = num + ' is odd.';
     console.log(num + ' is odd.');
 }
+
+myfs.appendFile('EvenOrOdd.txt', (s+'\n'), (e)=>{
+    if(e)throw e;
+})
